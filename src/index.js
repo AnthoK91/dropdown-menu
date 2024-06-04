ce93872c2d297a8cfcbe60ec3e4b98c4b6fae087
Anthony Kirkham <antho.kirkham@gmail.com>
2024-06-04 22:02:43 +1000

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+const dropdown = document.body.querySelector(".dropdown");
+const dropdownList = document.querySelector(".dropdown-list");
+
+dropdown.addEventListener("click", () => {
+  if (dropdownList.className === "dropdown-list") {
+    console.log("dropdown-item");
+    dropdownList.classList.remove("dropdown-list");
+    dropdownList.classList.add("dropdown-item-hide");
+  } else {
+    dropdownList.classList.add("dropdown-list");
+    dropdownList.classList.remove("dropdown-item-hide");
+  }
+});
+
+console.log("hello world");
